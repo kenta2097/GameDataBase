a041f5830a29f862c36f843788f95f9714863453
Alberto Muriel <alberto.muriel@se.com>
2025-03-07 21:13:55 +0100

    TEST 05

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { BrowserRouter as Router } from 'react-router-dom'
+
+function App() {
+  return (
+    <Router basename="/GameDataBase">
+      <div>
+        <h1>Game Database</h1>
+      </div>
+    </Router>
+  )
+}
+
+export default App
