31f928f1a88e23306e8a568df8462666d39d37ef
Alberto Muriel <alberto.muriel@se.com>
2025-03-07 21:05:28 +0100

    TEST 01

---
+++ vite.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/GameDataBase/', // Asume que el nombre del repositorio es GameDataBase
+})
