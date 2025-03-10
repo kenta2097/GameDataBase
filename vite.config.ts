1c136d2278e1eea47e208978171b29b4ae5abd35
Alberto Muriel <alberto.muriel@se.com>
2025-03-10 12:07:08 +0100

    Updating tag-generator2

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/GameDataBase/', // Asume que el nombre del repositorio es GameDataBase
-})
