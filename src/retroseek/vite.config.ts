c801100b275ca8905dd1a1b27c6e216bf26a3097
Alberto Muriel <alberto.muriel@se.com>
2025-03-10 15:38:34 +0100

    fix: correct script path in index.html and update Vite config for GitHub Pages deployment

--- src/retroseek/vite.config.ts
+++ src/retroseek/vite.config.ts
@@ -7,6 +7,11 @@ export default defineConfig({
   base: '/GameDataBase/', // Asegúrate de que la base esté configurada correctamente para GitHub Pages
   build: {
     outDir: 'build', // Asegúrate de que el directorio de salida sea correcto
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html')
+      }
+    }
   },
   server: {
     port: 5173,
